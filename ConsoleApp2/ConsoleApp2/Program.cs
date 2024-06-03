c6fae0041dd2f676030f71bf18f3ad77d15bade1
dim4ik9228 <dmytro.stepaniuk@oa.edu.ua>
2024-06-03 17:29:15 +0300

    solution folder renamed

--- ConsoleApp2/ConsoleApp2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConsoleApp2;
-
-// The client code.
-var s1 = Singleton.GetInstance();
-var s2 = Singleton.GetInstance();
-
-Console.WriteLine(s1 == s2
-    ? "Singleton works, both variables contain the same instance."
-    : "Singleton failed, variables contain different instances.");
